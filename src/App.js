fc9ca9099706e167d3994e9ab75b4d271002d5b9
jays29 <juangnr@gmail.com>
2021-02-25 12:46:51 -0300

    Integrated Firebase. Created Sing In page and component. Added Sign in with Google

--- src/App.js
+++ src/App.js
@@ -6,17 +6,44 @@ import "./App.css"
 import Homepage from "./pages/hompage/homepage.component"
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
+import Authentication from "./pages/authentication/authentication"
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop" component={ShopPage} />
-      </Switch>
-    </div>
-  )
+import { auth } from "./firebase/firebase.utils"
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/authentication" component={Authentication} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ src/pages/authentication/authentication.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./authentication.styles.scss"
+import SignIn from "../../components/sign-in/sign-in.component"
+
+const Authentication = () => {
+  return (
+    <div className="authentication">
+      <SignIn />
+    </div>
+  )
+}
+
+export default Authentication
