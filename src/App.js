1908e8faabe91e4aaa1dafc8652e85c420ebcca2
jays29 <juangnr@gmail.com>
2021-03-09 12:54:36 -0300

    Added Chechout Page, Chechout-Item Component. Updated Cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Switch, Route, Redirect } from "react-router-dom"
 import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect"
 
 import "./App.css"
 
@@ -8,8 +9,10 @@ import Homepage from "./pages/hompage/homepage.component"
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
 import Authentication from "./pages/authentication/authentication"
+import CheckoutPage from "./pages/checkout/checkout.component"
 
 import { setCurrentUser } from "./redux/user/user.actions"
+import { selectCurrentUser } from "./redux/user/user.selector"
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
@@ -48,6 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/authentication"
@@ -61,8 +65,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
