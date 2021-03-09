bea73bbc72ca4249fc01efbca9eaaa9ef856e69d
jays29 <juangnr@gmail.com>
2021-03-09 15:44:16 -0300

    Added addItem and removeItem to CheckoutItem Component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -14,14 +14,15 @@ import "./cart-dropdown.styles.scss"
 const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
-      {cartItems.length ? (
-        cartItems.map(cartItem => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))
-      ) : (
-        <span className="empty-message">Your cart is empty</span>
-      )}
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
       <CustomButton
         onClick={() => {
           history.push("/checkout")
