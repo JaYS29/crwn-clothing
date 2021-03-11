cd110debd9b45b394445c060d0d35e7ad413d8fa
jays29 <juangnr@gmail.com>
2021-03-11 15:14:57 -0300

    Edited test credit card data explanation

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -41,7 +41,7 @@ const CheckoutPage = ({ cartItems, total }) => {
       </div>
       <div className="test-warning">
         * Please use the following credit card for payments * <br />
-        4242 4242 4242 4242 - Exp.: 01/currentYear - CVV: 123
+        4242 4242 4242 4242 - Exp.: 01/nextYear - CVV: 123
       </div>
       <StripeCheckoutButton price={total} />
     </div>
